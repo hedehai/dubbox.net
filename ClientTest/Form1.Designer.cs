a4a9eab2231232ef59bd925c5e1b212633d49d18
leo <254213048@qq.com>
2016-03-25 21:46:58 +0800

    initial commit2

--- ClientTest/Form1.Designer.cs
+++ ClientTest/Form1.Designer.cs
@@ -33,11 +33,11 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(321, 232);
+            this.button1.Location = new System.Drawing.Point(282, 227);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(122, 54);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "测试";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
--- ClientTest/Form1.cs
+++ ClientTest/Form1.cs
@@ -14,29 +14,32 @@ namespace ClientTest
 {
     public partial class Form1 : Form
     {
+       
+        RoundRobbinsManager _manager = RoundRobbinsManager.GetInstance();
+        
+
         public Form1()
         {
             InitializeComponent();
         }
 
 
-
-        RoundRobbinsManager _manager = RoundRobbinsManager.GetInstance();
-
         private void button1_Click(object sender, EventArgs e)
         {
+            
+            Console.WriteLine("\n*****************");
+            
             string host = _manager["com.alibaba.dubbo.demo.user.facade.AnotherUserRestService"].GetHost();
             Console.WriteLine(host);
 
             HttpClient client = new HttpClient();
 
-
-            string url = "http://" + host + "/services/users/1.json"; //http://172.16.0.105:8888/services/users/1.json
-
-            string result = client.GetStringAsync(url).Result;
-            Console.WriteLine(result);
-
-
+            if (host !=null)
+            {
+                string url = "http://" + host + "/services/users/1.json"; // eg. http://172.16.0.105:8888/services/users/1.json
+                string result = client.GetStringAsync(url).Result;
+                Console.WriteLine(result);
+            }
 
         }
     }
